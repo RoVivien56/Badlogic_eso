5226003f1ffa6ef03b78abbeb5dc11acd0b7ca06
Rozsnyói Vivien <rozsnyoiv56@gmail.com>
2022-02-10 17:43:36 +0100

    MainGame

---
+++ core/src/hu/csanyzeg/master/MainGame/MainGame.java
@@ -0,0 +1,19 @@
+package hu.csanyzeg.master.MainGame;
+
+import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
+
+public class MainGame extends MyGame {
+    public MainGame(boolean debug) {
+        super(debug);
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void create() {
+        super.create();
+    }
+}
